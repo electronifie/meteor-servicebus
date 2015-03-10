625da8b247318f3e4223e398669a76dc822783c2
Stefan Kutko <sikutko@gmail.com>
2015-03-10 14:58:05 -0400

    updating servicebus

--- package.js
+++ package.js
@@ -1,15 +1,14 @@
 Package.describe({
   summary: "Distrubuted service communication via servicebus architecture",
-  version: "0.0.5",
+  version: "0.0.6",
   git: "https://github.com/electronifie/meteor-servicebus.git"
 });
 
 Npm.depends({
-  "servicebus": "1.0.0-rc6"
+  "servicebus": "1.0.0"
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('METEOR@0.9.0');
   api.addFiles('electronifie:servicebus.js', 'server');
 });
 
